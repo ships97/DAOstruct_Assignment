3b673012d17402b9d937a1635c74f48743d0029c
ships97 <shiprasingh4796@gmail.com>
2022-12-30 00:39:40 +0530

    progress

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+// import Demo from './Pages/Demo';
 import Nasa from './Pages/Nasa';
 
 function App() {
   return (
     <div className="App">
       <Nasa/>
+      {/* <Demo /> */}
     </div>
   );
 }
--- src/Pages/Nasa.jsx
+++ src/Pages/Nasa.jsx
@@ -1,12 +1,39 @@
-import { Box } from '@chakra-ui/react';
-import React from 'react';
+import { Box,Grid,GridItem,Image } from '@chakra-ui/react';
+import React, { useEffect } from 'react';
 import Header from "../Component/Header";
+import { useState } from 'react';
+import axios from 'axios';
 
 const Nasa = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get(`https://api.nasa.gov/planetary/apod?api_key=gaff4Pwpu0Qg6woyFty1YhVRxhj4In1ImvOCyFD7&start_date=2022-10-01&end_date=2022-10-29&thumbs=true`)
+      .then((res) => {
+        console.log(res.data);
+        setData(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }, []);
+
+
   return (
     <>
       <Box>
         <Header />
+        <Box>
+          <Grid>
+            {data.map((e,i) => {
+              return (
+                <GridItem key={i}>
+                  <Image src={e.url} alt="image" />
+                </GridItem>
+              )
+            })}
+          </Grid>
+        </Box>
       </Box>
     </>
   )
