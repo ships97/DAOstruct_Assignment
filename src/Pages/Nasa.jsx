2c86ea148689ab4f5a78896650c73353e1f8e5bf
ships97 <shiprasingh4796@gmail.com>
2022-12-31 21:45:05 +0530

    progress

--- src/Pages/Nasa.jsx
+++ src/Pages/Nasa.jsx
@@ -1,24 +1,48 @@
-import { Box,Grid,GridItem,Heading,Image, Text } from '@chakra-ui/react';
-import React, { useEffect } from 'react';
+import {
+  Box,
+  Grid,
+  GridItem,
+  Heading,
+  Image,
+  Skeleton,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
+import React, { useEffect } from "react";
 import Header from "../Component/Header";
-import { useState } from 'react';
-import axios from 'axios';
-import styles from '../Styles/Nasa.module.css';
+import { useState } from "react";
+import axios from "axios";
+import styles from "../Styles/Nasa.module.css";
+import SpotLight from "./SpotLight";
 
 const Nasa = () => {
   const [data, setData] = useState([]);
+  const [spotLightData, setSpotLightData] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const handleMainData = async () => {
+    setLoading(true);
+    try {
+      const res = await axios.get(
+        `https://api.nasa.gov/planetary/apod?api_key=gaff4Pwpu0Qg6woyFty1YhVRxhj4In1ImvOCyFD7&start_date=2022-10-01&end_date=2022-10-29&thumbs=true`
+      );
+      setError(false);
+      setData(res.data);
+    } catch (error) {
+      console.log(error);
+      setError(true);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
-    axios.get(`https://api.nasa.gov/planetary/apod?api_key=gaff4Pwpu0Qg6woyFty1YhVRxhj4In1ImvOCyFD7&start_date=2022-10-01&end_date=2022-10-29&thumbs=true`)
-      .then((res) => {
-        console.log(res.data);
-        setData(res.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      })
+    handleMainData();
   }, []);
 
+  const handleSpotLightData = (index) => {
+    setSpotLightData(data[index]);
+  };
 
   return (
     <>
@@ -26,35 +50,66 @@ const Nasa = () => {
         <Header />
         <Box className={styles.main}>
           <Heading>Spotlight section</Heading>
-          <Box className={styles.up}>
-            <Heading size={"md"}>Lunation Matrix</Heading>
-            <Text style={{overflow:"hidden",textOverflow:"ellipsis",whiteSpace: "nowrap", width: "350px"}}>LOREM</Text>
-            <Text size={"md"}>Author :- Tunc Tezel</Text>
-          </Box>
-          <Box className={styles.up1}>
-            <Image className={styles.imag} src="" alt="moon" />
-          </Box>
+          <SpotLight data={spotLightData} /> 
         </Box>
         <Box>
-          <Grid className={styles.sub}>
-            {data.map((e,i) => {
-              return (
-                <GridItem className={styles.mid} key={i}>
-                  {
-                    e.media_type === "image" ? <Image className={styles.imag1} src={e.hdurl} alt="image" /> : <iframe className={styles.imag1} src={e.url} alt="video" />
-                  }
-                  <Heading size={"md"}>{e.title} , {e.date}</Heading>
-                  {/* <Text style={{overflow:"hidden",textOverflow:"ellipsis",whiteSpace: "nowrap", width: "350px"}}>{e.explanation}</Text> */}
-                  <Text size={"md"}>Author :- {e.copyright}</Text>
-                  {/* <Text>Media-Type :- {e.media_type}</Text> */}
-                </GridItem>
-              )
-            })}
-          </Grid>
+          {!error ? (
+            <Grid
+              className={styles.sub}
+              templateColumns={[
+                "repeat(1, 1fr)",
+                "repeat(2, 1fr)",
+                "repeat(3, 1fr)",
+                "repeat(7, 1fr)",
+              ]}
+            >
+              {loading
+                ? new Array(7).fill(0).map((e, i) => (
+                    <GridItem className={styles.mid} key={i}>
+                      <Stack>
+                        <Skeleton height="300px" bg={"grey"} />
+                        <Skeleton height="20px" />
+                        <Skeleton height="20px" />
+                      </Stack>
+                    </GridItem>
+                  ))
+                : data?.map((e, i) => {
+                    return (
+                      <GridItem className={styles.mid} key={i}>
+                        {e.media_type === "image" ? (
+                          <Image
+                            className={styles.imag1}
+                            src={e.hdurl}
+                            alt="image"
+                            onClick={() => handleSpotLightData(i)}
+                          />
+                        ) : (
+                          <iframe
+                            className={styles.imag1}
+                            src={e.url}
+                            alt="video"
+                            onClick={() => handleSpotLightData(i)}
+                          />
+                        )}
+                        <Heading size={"md"}>
+                          {e.title} , {e.date}
+                        </Heading>
+                        {/* <Text style={{overflow:"hidden",textOverflow:"ellipsis",whiteSpace: "nowrap", width: "350px"}}>{e.explanation}</Text> */}
+                        <Text size={"md"}>
+                          {e.copyright && `Author :- ${e.copyright}`}
+                        </Text>
+                        {/* <Text>Media-Type :- {e.media_type}</Text> */}
+                      </GridItem>
+                    );
+                  })}
+            </Grid>
+          ) : (
+            <Box>Something went wrong ...</Box>
+          )}
         </Box>
       </Box>
     </>
-  )
-}
+  );
+};
 
 export default Nasa;
--- src/Pages/SpotLight.jsx
+++ src/Pages/SpotLight.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import { Box, Image } from "@chakra-ui/react";
+import React from "react";
+import styles from "../Styles/Nasa.module.css";
 
-const SpotLight = () => {
+const SpotLight = ({ data }) => {
+  // console.log(data);
   return (
     <>
       <Box>
-        
+        {Object.keys(data).length > 0 ? (
+          data?.media_type === "image" ? (
+            <Image className={styles.imag1} src={data?.hdurl} alt="image" />
+          ) : (
+            <iframe className={styles.imag1} src={data?.url} alt="video" />
+          )
+        ) : (
+          ""
+        )}
       </Box>
     </>
-  )
-}
+  );
+};
 
 export default SpotLight;
